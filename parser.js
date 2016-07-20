0bde4742a6af6c173b27ff906f32298a1f7fe8d8
Alex <alexellis2@gmail.com>
2016-07-20 09:03:54 +0100

    Parse out new lines

--- parser.js
+++ parser.js
@@ -9,11 +9,47 @@ class Parser {
       return text;
     }
 
+    popFullstop(text) {
+      if(text.endsWith(".")) {
+        return text.substring(0, text.length-1);
+      }
+      return text;
+    }
+
+
+    popQuote(text) {
+      if(text.endsWith("\"")) {
+        return text.substring(0, text.length-1);
+      }
+      return text;
+    }
+
+    splitWhitespace(text) {
+        let parts = text.split(" ");
+        let allParts = [];
+
+        parts.forEach((p) => {
+          if(p.indexOf("\n") > -1) {
+            let intermediates = p.split("\n");
+            intermediates.forEach((intermediate) => {
+              allParts.push(intermediate);
+            });
+          } else {
+            allParts.push(p);
+          }
+        });
+
+        return allParts;
+    }
+
     parse(text) {
-        let splits =  text.split(" ");
+        let splits =  this.splitWhitespace(text);
         let tokens = [];
         splits.forEach((t)=> {
           let token = this.popComma(t);
+          token = this.popFullstop(token);
+          token = this.popQuote(token);
+          
           if(token) {
             tokens.push(token);
           }
--- test/parser_tests.js
+++ test/parser_tests.js
@@ -9,6 +9,13 @@ describe("Parses strings", () => {
         sut = new Parser();
     });
 
+    it("by separating newlines into tokens", () => {
+        var tokens = sut.parse("a\nb");
+        expect(tokens.length).to.equal(2);
+        expect(tokens[0]).to.equal("a");
+        expect(tokens[1]).to.equal("b");
+    });
+
     it("by splitting out spaces into tokens", () => {
         var tokens = sut.parse("a b");
         expect(tokens.length).to.equal(2);
@@ -16,13 +23,30 @@ describe("Parses strings", () => {
         expect(tokens[1]).to.equal("b");
     });
 
-      it("by removing commas", () => {
-          var tokens = sut.parse("a, b");
+    it("by removing quote (\")", () => {
+        var tokens = sut.parse("b\"");
 
-          expect(tokens.length).to.equal(2);
+        expect(tokens.length).to.equal(1);
 
-          expect(tokens[0]).to.equal("a");
-          expect(tokens[1]).to.equal("b");
-      });
+        expect(tokens[0]).to.equal("b");
+    });
+
+    it("by removing period (.)", () => {
+        var tokens = sut.parse("a. b");
+
+        expect(tokens.length).to.equal(2);
+
+        expect(tokens[0]).to.equal("a");
+        expect(tokens[1]).to.equal("b");
+    });
+
+    it("by removing commas (,)", () => {
+        var tokens = sut.parse("a, b");
+
+        expect(tokens.length).to.equal(2);
+
+        expect(tokens[0]).to.equal("a");
+        expect(tokens[1]).to.equal("b");
+    });
 
 });
